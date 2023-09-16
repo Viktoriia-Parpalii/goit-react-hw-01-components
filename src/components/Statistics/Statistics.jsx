360c7a803065c8c0647aae335c0df5b27f0e3948
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2023-09-16 19:12:42 +0300

    fix Statistic and changed style

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ import { StatisticList } from 'components/StatisticList/StatisticList';
 export const Statistics = ({title , stats}) => 
 {
    return ( <section className={css.statistics}>
-            <h2 className={css.title}>{title}</h2>
+           { title && <h2 className={css.title}>{title}</h2>}
             <ul className={css['stat-list']}>
            {stats.map((stat) => {
                return (<StatisticList  key={stat.id} id={stat.id} label={stat.label} percentage={stat.percentage}/>)
